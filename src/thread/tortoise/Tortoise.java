0559c52d62f57a77c639ef8fcebea6138331a9c2
MukulRajora <68554850+MukulRajora@users.noreply.github.com>
2020-10-29 21:47:25 +0530

    commit 3

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -13,6 +13,33 @@ public class Tortoise extends Thread {private static final int MILESTONES = 5;
     public Thread getTortoise() {
         return tortoise;
     }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+
+            }
+
+
+
+
+
 }
 
 
